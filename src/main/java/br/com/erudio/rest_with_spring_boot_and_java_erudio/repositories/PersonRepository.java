d6672f6e43a1474f3aee15b4e80b3a3fe2bd6a3a
luizalec7 <luiz.alecsander@gmail.com>
2025-01-14 16:48:58 -0300

    Seção 10: versionamento

--- src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.erudio.rest_with_spring_boot_and_java_erudio.repositories;
-
-import br.com.erudio.rest_with_spring_boot_and_java_erudio.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
